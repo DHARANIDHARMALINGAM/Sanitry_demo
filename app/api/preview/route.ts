7497ac928897229ed4c91262a77c423f21ccac98
DHARANIDHARMALINGAM <dharaniidharmalingam@gmail.com>
2026-01-22 21:49:20 +0530

    error fixed

--- app/api/preview/route.ts
+++ app/api/preview/route.ts
@@ -2,7 +2,8 @@ import { draftMode } from 'next/headers'
 import { redirect } from 'next/navigation'
 
 export async function GET(request: Request) {
-  draftMode().enable()
+  const draft = await draftMode(); // Await the promise
+  draft.enable();     
 
   const { searchParams } = new URL(request.url)
   const slug = searchParams.get('slug')
